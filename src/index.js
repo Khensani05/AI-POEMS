cb8777d8f5886251e6cb035345cac70cb9ae3b33
Khensani05 <notigokhensani@gmail.com>
2025-01-20 10:17:58 +0200

    Added the API intergration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
+function displayPoem(response) {
+  console.log("Poem Generated!!");
+  new Typewriter("#thepoem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
 function generatepoem(event) {
   event.preventDefault();
 
-  new Typewriter("#thepoem", {
-    strings:
-      "Dyambu arixa rithlela ripela Tinguva ticica, ku hisa kuthlela ku titimela",
-    autoStart: true,
-    delay: 1,
-    cursor: "|",
-  });
+  let instructions = document.querySelector("#txtTopic");
+  let apiKey = "b03c74bff4oa46bc3d3tee6230146d23";
+  let prompt = `user instructions: Generate a poem in XiTsonga language about ${txtTopic.value}`;
+  let context =
+    "Make it sweet, kind and short. Make sure to follow user instructions. Just write the generate the poem alone. Please seperate the lines by a <br />";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Poem...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemForm = document.querySelector("#poemGenerator");
