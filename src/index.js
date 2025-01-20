9987946b69eecf7297f87ac2bf35c395c4bcee59
Khensani05 <notigokhensani@gmail.com>
2025-01-20 09:31:04 +0200

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatepoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#thepoem", {
+    strings:
+      "Dyambu arixa rithlela ripela Tinguva ticica, ku hisa kuthlela ku titimela",
+    autoStart: true,
+    delay: 1,
+    cursor: "|",
+  });
+}
+
+let poemForm = document.querySelector("#poemGenerator");
+poemForm.addEventListener("submit", generatepoem);
